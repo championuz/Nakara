e62d50104597712bd8e2537593f6a2d9b63b5891
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-02-10 14:12:56 +0100

    functionality to check when an token has expired

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ router.post('/login', async(req, res) => {
       id: user._id,
       isAdmin: user.isAdmin
     }, process.env.JWT_SECT,
-      {expiresIn: '3d'}
+      {expiresIn: '1d'}
     )
 
     const {password, ...others} = user._doc
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,11 +2,13 @@ const jwt = require('jsonwebtoken')
 
 // checking for validity of token
 const verifyToken = (req, res, next) => {
-  const authHeader= req.headers.token
+  const authHeader = req.headers.token
   if(authHeader){
     const token = authHeader.split(' ')[1]
+    const exp = jwt.decode(token).exp
+    if (Date.now() >= exp * 1000) return res.status(403).json({status:'error', expired: true, message: 'Your session has expired'})
     jwt.verify(token, process.env.JWT_SECT, (err, user) => {
-      if(err) return res.status(403).json('Token is not valid')
+      if(err) return res.status(403).json({status: 'error', message: 'Token is invalid'})
       req.user = user
       next()
     })
